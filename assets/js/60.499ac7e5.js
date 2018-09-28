e7b1b0ca89fd57dfe1b65754665aaeb9242e89ac
Semaphore Directus Auto Deploy <deploy@semaphoreci.com>
2018-09-28 15:58:58 +0000

    Auto deploy

---
+++ assets/js/60.499ac7e5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[60],{210:function(t,n,e){"use strict";e.r(n);var s=e(0),a=Object(s.a)({},function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"content"})},[],!1,null,null,null);a.options.__file="create-translation.md";n.default=a.exports}}]);
